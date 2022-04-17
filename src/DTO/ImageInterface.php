f7e7499a0f4b80d091dbdfe128bd12d9f02d4e3c
Jan Barášek <janbarasek@gmail.com>
2022-04-17 10:35:14 +0200

    ImageInterface: Add definitions.

--- src/DTO/ImageInterface.php
+++ src/DTO/ImageInterface.php
@@ -5,7 +5,30 @@ declare(strict_types=1);
 namespace Baraja\EcommerceStandard\DTO;
 
 
-interface ImageInterface
+interface ImageInterface extends \Stringable
 {
+	public function getId(): int;
+
 	public function getRelativePath(): string;
+
+	public function getSource(): string;
+
+	public function getUrl(): string;
+
+	/**
+	 * @return array{source: string, title: string, url: string}
+	 */
+	public function toArray(): array;
+
+	public function getProduct(): ProductInterface;
+
+	public function getVariant(): ?ProductVariantInterface;
+
+	public function getPosition(): int;
+
+	public function getAltTitle(): string;
+
+	public function getTag(): ?string;
+
+	public function setTag(?string $tag): void;
 }
